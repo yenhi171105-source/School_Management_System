10e60e6aa29e960d7e4c5bd40dce9121981000b9
yenhi171105-source <yenhi171105@gmail.com>
2026-02-24 19:23:02 +0700

    Add files via upload

---
+++ TeacherScoreForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SchoolMangementSystem
+{
+    partial class TeacherScoreForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.ComboBox cbClass;
+        private System.Windows.Forms.ComboBox cbSubject;
+        private System.Windows.Forms.ComboBox cbSemester;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblSemester;
+        private System.Windows.Forms.DataGridView dgvScores;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.cbClass = new System.Windows.Forms.ComboBox();
+            this.cbSubject = new System.Windows.Forms.ComboBox();
+            this.cbSemester = new System.Windows.Forms.ComboBox();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblSemester = new System.Windows.Forms.Label();
+            this.dgvScores = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvScores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.cbClass);
+            this.panelTop.Controls.Add(this.cbSubject);
+            this.panelTop.Controls.Add(this.cbSemester);
+            this.panelTop.Controls.Add(this.lblClass);
+            this.panelTop.Controls.Add(this.lblSubject);
+            this.panelTop.Controls.Add(this.lblSemester);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1200, 60);
+            this.panelTop.TabIndex = 3;
+            // 
+            // cbClass
+            // 
+            this.cbClass.Location = new System.Drawing.Point(65, 17);
+            this.cbClass.Name = "cbClass";
+            this.cbClass.Size = new System.Drawing.Size(121, 24);
+            this.cbClass.TabIndex = 0;
+            // 
+            // cbSubject
+            // 
+            this.cbSubject.Enabled = false;
+            this.cbSubject.Location = new System.Drawing.Point(283, 17);
+            this.cbSubject.Name = "cbSubject";
+            this.cbSubject.Size = new System.Drawing.Size(121, 24);
+            this.cbSubject.TabIndex = 1;
+            // 
+            // cbSemester
+            // 
+            this.cbSemester.Location = new System.Drawing.Point(499, 17);
+            this.cbSemester.Name = "cbSemester";
+            this.cbSemester.Size = new System.Drawing.Size(121, 24);
+            this.cbSemester.TabIndex = 2;
+            // 
+            // lblClass
+            // 
+            this.lblClass.Location = new System.Drawing.Point(20, 20);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(100, 23);
+            this.lblClass.TabIndex = 3;
+            this.lblClass.Text = "Class";
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.Location = new System.Drawing.Point(220, 20);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(100, 23);
+            this.lblSubject.TabIndex = 4;
+            this.lblSubject.Text = "Subject";
+            // 
+            // lblSemester
+            // 
+            this.lblSemester.Location = new System.Drawing.Point(428, 20);
+            this.lblSemester.Name = "lblSemester";
+            this.lblSemester.Size = new System.Drawing.Size(100, 23);
+            this.lblSemester.TabIndex = 5;
+            this.lblSemester.Text = "Semester";
+            // 
+            // dgvScores
+            // 
+            this.dgvScores.AllowUserToAddRows = false;
+            this.dgvScores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvScores.ColumnHeadersHeight = 29;
+            this.dgvScores.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvScores.Location = new System.Drawing.Point(0, 60);
+            this.dgvScores.Name = "dgvScores";
+            this.dgvScores.RowHeadersWidth = 51;
+            this.dgvScores.Size = new System.Drawing.Size(1200, 400);
+            this.dgvScores.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSave.Location = new System.Drawing.Point(400, 480);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(120, 35);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnClear.Location = new System.Drawing.Point(550, 480);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 35);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // TeacherScoreForm
+            // 
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgvScores);
+            this.Controls.Add(this.panelTop);
+            this.Name = "TeacherScoreForm";
+            this.Size = new System.Drawing.Size(1200, 600);
+            this.panelTop.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvScores)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ TeacherScoreForm.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static SchoolMangementSystem.ManagerAssignForm;
+
+namespace SchoolMangementSystem
+{
+    public partial class TeacherScoreForm : UserControl
+    {
+        SqlConnection connect = new SqlConnection(
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True");
+
+        int teacherId;
+        bool isManager = false;
+
+        public TeacherScoreForm(bool managerMode = false)
+        {
+            InitializeComponent();
+            teacherId = GetTeacherIdByUser();
+            isManager = managerMode;
+            
+            LoadSemesters();
+            LoadClasses();
+
+            cbClass.SelectedIndexChanged += cbClass_SelectedIndexChanged;
+            cbSemester.SelectedIndexChanged += cbSemester_SelectedIndexChanged;
+            btnSave.Click += btnSave_Click;
+            btnClear.Click += btnClear_Click;
+
+            ApplyPermission();
+        }
+
+        void LoadSemesters()
+        {
+            cbSemester.Items.Clear();
+            cbSemester.Items.Add("1");
+            cbSemester.Items.Add("2");
+            cbSemester.SelectedIndex = -1;
+        }
+
+        void LoadClasses()
+        {
+            string sql;
+
+            if (isManager)
+            {
+                sql = "SELECT class_id, class_name FROM classes ORDER BY class_name";
+            }
+            else
+            {
+                sql = @"SELECT DISTINCT c.class_id, c.class_name
+                FROM teaching_assignments ta
+                JOIN classes c ON ta.class_id = c.class_id
+                WHERE ta.teacher_id = @tid";
+            }
+
+            using (SqlDataAdapter da = new SqlDataAdapter(sql, connect))
+            {
+                if (!isManager)
+                    da.SelectCommand.Parameters.AddWithValue("@tid", teacherId);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                cbClass.DataSource = dt;
+                cbClass.DisplayMember = "class_name";
+                cbClass.ValueMember = "class_id";
+                cbClass.SelectedIndex = -1;
+            }
+        }
+
+        void LoadSubject(int classId)
+        {
+            cbSubject.Items.Clear();
+            bool isHomeroom = IsHomeroomTeacher(classId);
+            if (isManager || isHomeroom)
+            {
+                LoadAllSubjects();
+                cbSubject.Enabled = true;
+            }
+            else
+            {
+                LoadOnlyTeacherSubject(classId);
+                cbSubject.Enabled = false;
+            }
+        }
+
+        void LoadStudents()
+        {
+            if (cbClass.SelectedIndex == -1 || cbSemester.SelectedIndex == -1)
+                return;
+
+            int classId = Convert.ToInt32(cbClass.SelectedValue);
+            int semester = Convert.ToInt32(cbSemester.Text);
+            bool isHomeroom = IsHomeroomTeacher(classId);
+            string sql;
+
+            if (isManager || isHomeroom)
+            {
+                if (cbSubject.SelectedItem == null)
+                {
+                    sql = @"
+                        SELECT 
+                            s.student_id,
+                            s.student_name,
+                            sub.subject_name,
+                            sc.score
+                        FROM students s
+                        CROSS JOIN subjects sub
+                        LEFT JOIN scores sc
+                            ON s.student_id = sc.student_id
+                           AND sc.class_id = @cid
+                           AND sc.subject_id = sub.subject_id
+                           AND sc.semester = @sem
+                        WHERE s.class_id = @cid
+                          AND s.date_delete IS NULL
+                        ORDER BY s.student_name, sub.subject_name";
+                }
+                else
+                {
+                    sql = @"
+                        SELECT 
+                            s.student_id,
+                            s.student_name,
+                            sub.subject_name,
+                            sc.score
+                        FROM students s
+                        CROSS JOIN subjects sub
+                        LEFT JOIN scores sc
+                            ON s.student_id = sc.student_id
+                           AND sc.class_id = @cid
+                           AND sc.subject_id = @sid
+                           AND sc.semester = @sem
+                        WHERE s.class_id = @cid
+                          AND s.date_delete IS NULL
+                          AND sub.subject_id = @sid
+                        ORDER BY s.student_name";
+                }
+            }
+            else
+            {
+                if (cbSubject.SelectedItem == null)
+                    return;
+
+                int subjectId = (int)((ComboboxItem)cbSubject.SelectedItem).Value;
+
+                sql = @"
+                    SELECT 
+                        s.student_id,
+                        s.student_name,
+                        sc.score
+                    FROM students s
+                    LEFT JOIN scores sc
+                        ON s.student_id = sc.student_id
+                       AND sc.class_id = @cid
+                       AND sc.subject_id = @sid
+                       AND sc.semester = @sem
+                       AND sc.teacher_id = @tid
+                    WHERE s.class_id = @cid
+                      AND s.date_delete IS NULL
+                    ORDER BY s.student_name";
+            }
+
+            using (SqlDataAdapter da = new SqlDataAdapter(sql, connect))
+            {
+                da.SelectCommand.Parameters.AddWithValue("@cid", classId);
+                da.SelectCommand.Parameters.AddWithValue("@sem", semester);
+
+                if (cbSubject.SelectedItem != null)
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@sid",
+                        ((ComboboxItem)cbSubject.SelectedItem).Value);
+                }
+
+                if (!isManager && !isHomeroom)
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@tid", teacherId);
+                }
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvScores.DataSource = dt;
+
+            }
+        }
+
+        private void cbClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbClass.SelectedIndex == -1) return;
+
+            int classId = Convert.ToInt32(cbClass.SelectedValue);
+            LoadSubject(classId);
+
+            if (cbSemester.SelectedIndex != -1)
+                LoadStudents();
+        }
+
+        private void cbSemester_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbClass.SelectedIndex != -1)
+                LoadStudents();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // 1. Manager không được sửa
+            if (isManager)
+            {
+                MessageBox.Show("Manager cannot edit scores.");
+                return;
+            }
+
+            // 2. Kiểm tra dữ liệu
+            if (dgvScores.DataSource == null)
+            {
+                MessageBox.Show("No data to save.");
+                return;
+            }
+
+            if (cbClass.SelectedIndex == -1 || cbSemester.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select class and semester.");
+                return;
+            }
+
+            if (cbSubject.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a subject.");
+                return;
+            }
+
+            int classId = Convert.ToInt32(cbClass.SelectedValue);
+            int semester = Convert.ToInt32(cbSemester.Text);
+            int subjectId = (int)((ComboboxItem)cbSubject.SelectedItem).Value;
+
+            // 3. Kiểm tra giáo viên có dạy môn này không
+            if (!IsTeacherAssignedToSubject(classId, subjectId))
+            {
+                MessageBox.Show("You can only edit the subject you teach.");
+                return;
+            }
+
+            try
+            {
+                connect.Open();
+
+                foreach (DataGridViewRow row in dgvScores.Rows)
+                {
+                    if (row.IsNewRow) continue;
+                    if (row.Cells["score"].Value == null) continue;
+
+                    decimal score;
+                    if (!decimal.TryParse(row.Cells["score"].Value.ToString(), out score))
+                        continue;
+
+                    if (score < 0 || score > 10)
+                    {
+                        MessageBox.Show("Score must be between 0 and 10.");
+                        connect.Close();
+                        return;
+                    }
+
+                    string studentId = row.Cells["student_id"].Value.ToString();
+
+                    SqlCommand cmd = new SqlCommand(@"
+                IF EXISTS (
+                    SELECT 1 FROM scores
+                    WHERE student_id = @sid
+                      AND class_id = @cid
+                      AND subject_id = @sub
+                      AND semester = @sem
+                )
+                UPDATE scores
+                SET score = @score,
+                    teacher_id = @tid,
+                    date_insert = GETDATE()
+                WHERE student_id = @sid
+                  AND class_id = @cid
+                  AND subject_id = @sub
+                  AND semester = @sem
+                ELSE
+                INSERT INTO scores
+                (student_id, class_id, subject_id, semester, attempt, score, teacher_id, date_insert)
+                VALUES
+                (@sid, @cid, @sub, @sem, 1, @score, @tid, GETDATE())
+            ", connect);
+
+                    cmd.Parameters.AddWithValue("@sid", studentId);
+                    cmd.Parameters.AddWithValue("@cid", classId);
+                    cmd.Parameters.AddWithValue("@sub", subjectId);
+                    cmd.Parameters.AddWithValue("@sem", semester);
+                    cmd.Parameters.AddWithValue("@score", score);
+                    cmd.Parameters.AddWithValue("@tid", teacherId);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Scores saved successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+            LoadStudents();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dgvScores.DataSource = null;
+            cbClass.SelectedIndex = -1;
+            cbSemester.SelectedIndex = -1;
+            cbSubject.Text = "";
+        }
+        int GetTeacherIdByUser()
+        {
+            using (SqlCommand cmd = new SqlCommand(
+                "SELECT id FROM teachers WHERE user_id = @uid", connect))
+            {
+                cmd.Parameters.AddWithValue("@uid", Session.UserId);
+                connect.Open();
+                object result = cmd.ExecuteScalar();
+                connect.Close();
+
+                return result == null ? 0 : Convert.ToInt32(result);
+            }
+        }
+        int GetTeacherSubjectId(int teacherDbId)
+        {
+            using (SqlCommand cmd = new SqlCommand(
+                "SELECT subject_id FROM teachers WHERE id = @tid", connect))
+            {
+                cmd.Parameters.AddWithValue("@tid", teacherDbId);
+                connect.Open();
+                object result = cmd.ExecuteScalar();
+                connect.Close();
+
+                return result == null ? 0 : Convert.ToInt32(result);
+            }
+        }
+        void ApplyPermission()
+        {
+            if (isManager)
+            {
+                btnSave.Visible = false;
+                dgvScores.ReadOnly = true;
+                cbSubject.Enabled = true;
+            }
+            else
+            {
+                btnSave.Visible = true;
+                dgvScores.ReadOnly = false;
+            }
+        }
+        bool IsHomeroomTeacher(int classId)
+        {
+            using (SqlCommand cmd = new SqlCommand(
+                @"SELECT COUNT(*) FROM classes 
+          WHERE class_id = @cid 
+            AND homeroom_teacher_id = @tid", connect))
+            {
+                cmd.Parameters.AddWithValue("@cid", classId);
+                cmd.Parameters.AddWithValue("@tid", teacherId);
+
+                connect.Open();
+                int count = (int)cmd.ExecuteScalar();
+                connect.Close();
+
+                return count > 0;
+            }
+        }
+        void LoadAllSubjects()
+        {
+            using (SqlCommand cmd = new SqlCommand(
+                "SELECT subject_id, subject_name FROM subjects", connect))
+            {
+                connect.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    cbSubject.Items.Add(new ComboboxItem(
+                        dr["subject_name"].ToString(),
+                        dr["subject_id"]
+                    ));
+                }
+                dr.Close();
+                connect.Close();
+            }
+        }
+        void LoadOnlyTeacherSubject(int classId)
+        {
+            using (SqlCommand cmd = new SqlCommand(
+                @"SELECT s.subject_id, s.subject_name
+                  FROM teaching_assignments ta
+                  JOIN subjects s ON ta.subject_id = s.subject_id
+                  WHERE ta.teacher_id = @tid
+                    AND ta.class_id = @cid", connect))
+            {
+                cmd.Parameters.AddWithValue("@tid", teacherId);
+                cmd.Parameters.AddWithValue("@cid", classId);
+
+                connect.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    var item = new ComboboxItem(
+                        dr["subject_name"].ToString(),
+                        dr["subject_id"]
+                    );
+
+                    cbSubject.Items.Add(item);
+                    cbSubject.SelectedItem = item;   
+                }
+
+                dr.Close();
+                connect.Close();
+            }
+        }
+        bool IsTeacherAssignedToSubject(int classId, int subjectId)
+        {
+            using (SqlCommand cmd = new SqlCommand(@"
+        SELECT COUNT(*) 
+        FROM teaching_assignments
+        WHERE teacher_id = @tid
+          AND class_id = @cid
+          AND subject_id = @sid", connect))
+            {
+                cmd.Parameters.AddWithValue("@tid", teacherId);
+                cmd.Parameters.AddWithValue("@cid", classId);
+                cmd.Parameters.AddWithValue("@sid", subjectId);
+
+                connect.Open();
+                int count = (int)cmd.ExecuteScalar();
+                connect.Close();
+
+                return count > 0;
+            }
+        }
+    }
+}
