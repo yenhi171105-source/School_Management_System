e2a2af3a1e78a4fbd68fa4545737a0a24015f0ae
yenhi171105-source <yenhi171105@gmail.com>
2026-02-24 19:14:55 +0700

    Add files via upload

---
+++ Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMangementSystem
+{
+    public static class Session
+    {
+        public static int UserId { get; set; }
+        public static string Username { get; set; }
+        public static string Role { get; set; }
+
+        // teacher only
+        public static int TeacherDbId { get; set; }
+        public static string TeacherID { get; set; }
+    }
+}
