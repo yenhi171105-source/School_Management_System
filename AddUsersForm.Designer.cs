b63eee8bcec2a52363a8d88c293d4ee189283631
Bé cò <phamtruongnd2005@gmail.com>
2026-02-24 19:12:25 +0700

    Add files via upload

---
+++ AddUsersForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace SchoolMangementSystem
+{
+    partial class AddUsersForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDeleteUser;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnDeleteUser = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(348, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(348, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 8;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(448, 49);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(200, 22);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(448, 99);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 22);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(87)))), ((int)(((byte)(122)))));
+            this.btnCreate.ForeColor = System.Drawing.Color.White;
+            this.btnCreate.Location = new System.Drawing.Point(448, 155);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(150, 39);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create Account";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.txtUsername);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnDeleteUser);
+            this.panel2.Controls.Add(this.btnCreate);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtPassword);
+            this.panel2.Location = new System.Drawing.Point(0, 374);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1134, 281);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.dgvUsers);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Location = new System.Drawing.Point(0, -4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1134, 356);
+            this.panel1.TabIndex = 9;
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.AllowUserToResizeRows = false;
+            this.dgvUsers.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvUsers.CausesValidation = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(87)))), ((int)(((byte)(122)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.EnableHeadersVisualStyles = false;
+            this.dgvUsers.Location = new System.Drawing.Point(27, 58);
+            this.dgvUsers.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvUsers.RowHeadersVisible = false;
+            this.dgvUsers.RowHeadersWidth = 51;
+            this.dgvUsers.Size = new System.Drawing.Size(1080, 276);
+            this.dgvUsers.TabIndex = 1;
+            this.dgvUsers.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_CellClick);
+            this.dgvUsers.CellContentClick += dgvUsers_CellContentClick;
+
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(21, 18);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(112, 24);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "User\'s Data";
+            // 
+            // btnDeleteUser
+            // 
+            this.btnDeleteUser.BackColor = System.Drawing.Color.DarkRed;
+            this.btnDeleteUser.ForeColor = System.Drawing.Color.White;
+            this.btnDeleteUser.Location = new System.Drawing.Point(448, 214);
+            this.btnDeleteUser.Name = "btnDeleteUser";
+            this.btnDeleteUser.Size = new System.Drawing.Size(150, 37);
+            this.btnDeleteUser.TabIndex = 11;
+            this.btnDeleteUser.Text = "Delete";
+            this.btnDeleteUser.UseVisualStyleBackColor = false;
+            this.btnDeleteUser.Click += new System.EventHandler(this.btnDeleteUser_Click);
+            // 
+            // AddUsersForm
+            // 
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "AddUsersForm";
+            this.Size = new System.Drawing.Size(1133, 654);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ AddUsersForm.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolMangementSystem
+{
+    public partial class AddUsersForm : UserControl
+    {
+        int selectedUserId = 0;
+        string selectedRole = "";
+
+        SqlConnection connect = new SqlConnection(
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True");
+
+        public AddUsersForm()
+        {
+            InitializeComponent();
+            LoadRoles();
+            LoadUsers();
+        }
+
+        // ================= LOAD ROLES =================
+        void LoadRoles()
+        {
+            try
+            {
+                connect.Open();
+                SqlDataAdapter da = new SqlDataAdapter(
+                    "SELECT role_id, role_name FROM roles WHERE role_name IN ('Manager', 'Teacher')", connect);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+
+        // ================= LOAD USERS =================
+        public void LoadUsers()
+        {
+            string query = @"
+                SELECT 
+                    u.id,
+                    u.username,
+                    r.role_name,
+                    u.is_active,
+                    u.date_create
+                FROM users u
+                JOIN roles r ON u.role_id = r.role_id";
+
+            SqlDataAdapter da = new SqlDataAdapter(query, connect);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvUsers.DataSource = dt;
+            dgvUsers.Columns["is_active"].ReadOnly = false;
+
+            ColorDisabledUsers();
+        }
+        void ColorDisabledUsers()
+        {
+            foreach (DataGridViewRow row in dgvUsers.Rows)
+            {
+                if (row.Cells["is_active"].Value != null &&
+                    Convert.ToInt32(row.Cells["is_active"].Value) == 0)
+                {
+                    row.DefaultCellStyle.BackColor = Color.LightGray;
+                    row.DefaultCellStyle.ForeColor = Color.DarkRed;
+                }
+            }
+        }
+
+
+        // ================= CREATE ACCOUNT =================
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (Session.Role != "Admin")
+            {
+                MessageBox.Show("Only admin can create accounts.");
+                return;
+            }
+
+            if (txtUsername.Text == "" || txtPassword.Text == "")
+            {
+                MessageBox.Show("Please fill all fields.");
+                return;
+            }
+
+            try
+            {
+                connect.Open();
+                int ROLE_MANAGER = 2;
+                SqlCommand checkManagerCmd = new SqlCommand(
+                    @"SELECT COUNT(*) 
+                      FROM users 
+                      WHERE role_id = @roleId AND is_active = 1",
+                    connect);
+
+                checkManagerCmd.Parameters.AddWithValue("@roleId", ROLE_MANAGER);
+
+                int managerCount = (int)checkManagerCmd.ExecuteScalar();
+
+                if (managerCount >= 3)
+                {
+                    MessageBox.Show("Only 3 Manager accounts are allowed!");
+                    connect.Close();
+                    return;
+                }
+
+                // Check duplicate username
+                SqlCommand check = new SqlCommand(
+                    "SELECT COUNT(*) FROM users WHERE username=@u", connect);
+                check.Parameters.AddWithValue("@u", txtUsername.Text.Trim());
+
+                if ((int)check.ExecuteScalar() > 0)
+                {
+                    MessageBox.Show("Username already exists!");
+                    return;
+                }
+
+                SqlCommand cmd = new SqlCommand(@"
+                    INSERT INTO users (username, password, role_id, is_active, date_create)
+                    VALUES (@u, @p, 2, 1, GETDATE())", connect);
+
+                cmd.Parameters.AddWithValue("@u", txtUsername.Text.Trim());
+                cmd.Parameters.AddWithValue("@p", txtPassword.Text.Trim()); 
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Account created successfully!");
+
+                LoadUsers();
+                ClearFields();
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+
+        // ================= GRID CLICK =================
+        private void dgvUsers_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            DataGridViewRow row = dgvUsers.Rows[e.RowIndex];
+
+            selectedUserId = Convert.ToInt32(row.Cells["id"].Value);
+            selectedRole = row.Cells["role_name"].Value.ToString();
+
+            txtUsername.Text = row.Cells["username"].Value.ToString();
+
+        }
+        private void dgvUsers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            // Chỉ xử lý cột is_active
+            if (dgvUsers.Columns[e.ColumnIndex].Name != "is_active") return;
+
+            DataGridViewRow row = dgvUsers.Rows[e.RowIndex];
+
+            int userId = Convert.ToInt32(row.Cells["id"].Value);
+            string username = row.Cells["username"].Value.ToString();
+            int currentStatus = Convert.ToInt32(row.Cells["is_active"].Value);
+
+            // Nếu đang bị disable và user muốn bật lại
+            if (currentStatus == 0)
+            {
+                DialogResult result = MessageBox.Show(
+                    $"Do you want to restore account '{username}'?",
+                    "Confirm Restore",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    RestoreUser(userId);
+                    LoadUsers();
+                    ColorDisabledUsers();
+
+                    MainForm main = (MainForm)this.FindForm();
+                    main.ReloadTeachers();
+
+                }
+                else
+                {
+                    // Hoàn tác checkbox
+                    row.Cells["is_active"].Value = 0;
+                }
+            }
+        }
+        void RestoreUser(int userId)
+        {
+            connect.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "UPDATE users SET is_active = 1 WHERE id = @id",
+                connect
+            );
+            cmd.Parameters.AddWithValue("@id", userId);
+
+            cmd.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        // ================= CLEAR =================
+        void ClearFields()
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+        private void btnDeleteUser_Click(object sender, EventArgs e)
+        {
+            if (selectedRole == "Admin")
+            {
+                MessageBox.Show("Admin account cannot be deleted");
+                return;
+            }
+
+            if (selectedUserId == 0)
+            {
+                MessageBox.Show("Please select a user first");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to delete this account?",
+                "Confirm",
+                MessageBoxButtons.YesNo
+            );
+
+            if (result != DialogResult.Yes) return;
+
+            connect.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                @"UPDATE users 
+                  SET is_active = 0 
+                  WHERE id = @id",
+                connect);
+
+            cmd.Parameters.AddWithValue("@id", selectedUserId);
+            cmd.ExecuteNonQuery();
+
+            connect.Close();
+
+            LoadUsers(); // reload grid
+            MainForm main = (MainForm)this.FindForm();
+            main.ReloadTeachers();
+            MessageBox.Show("User disabled successfully");
+        }
+        
+
+    }
+}
