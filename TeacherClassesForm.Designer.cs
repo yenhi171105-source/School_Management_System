ae3b8b1df805fe5245942a63f706dc657a5a6eaa
ngoc232 <ngoclaconong4@gmail.com>
2026-02-24 19:26:53 +0700

    Add files via upload

---
+++ TeacherClassesForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SchoolMangementSystem
+{
+    partial class TeacherClassesForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private Label lblTitle;
+        private Label lblClass;
+        private ComboBox cbClass;
+        private DataGridView dgvStudents;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.cbClass = new System.Windows.Forms.ComboBox();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(900, 60);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "My Classes";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblClass
+            // 
+            this.lblClass.AutoSize = true;
+            this.lblClass.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblClass.Location = new System.Drawing.Point(25, 75);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(64, 28);
+            this.lblClass.TabIndex = 2;
+            this.lblClass.Text = "Class:";
+            // 
+            // cbClass
+            // 
+            this.cbClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbClass.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.cbClass.Location = new System.Drawing.Point(90, 72);
+            this.cbClass.Name = "cbClass";
+            this.cbClass.Size = new System.Drawing.Size(96, 33);
+            this.cbClass.TabIndex = 1;
+            this.cbClass.SelectedIndexChanged += new System.EventHandler(this.cbClass_SelectedIndexChanged);
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.AllowUserToAddRows = false;
+            this.dgvStudents.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvStudents.BackgroundColor = System.Drawing.Color.White;
+            this.dgvStudents.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvStudents.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvStudents.ColumnHeadersHeight = 45;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvStudents.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvStudents.EnableHeadersVisualStyles = false;
+            this.dgvStudents.Location = new System.Drawing.Point(25, 120);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.ReadOnly = true;
+            this.dgvStudents.RowHeadersVisible = false;
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.RowTemplate.Height = 40;
+            this.dgvStudents.Size = new System.Drawing.Size(850, 420);
+            this.dgvStudents.TabIndex = 0;
+            // 
+            // TeacherClassesForm
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.cbClass);
+            this.Controls.Add(this.lblClass);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "TeacherClassesForm";
+            this.Size = new System.Drawing.Size(900, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ TeacherClassesForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolMangementSystem
+{
+    public partial class TeacherClassesForm : UserControl
+    {
+        public TeacherClassesForm()
+        {
+            InitializeComponent();
+            SetupStudentGrid();
+            LoadTeacherClasses();
+        }
+
+        private void LoadTeacherClasses()
+        {
+            using (SqlConnection conn =
+                new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True"))
+            {
+                conn.Open();
+
+                string sql = @"
+            SELECT DISTINCT c.class_id, c.class_name
+            FROM teaching_assignments ta
+            INNER JOIN classes c 
+                ON ta.class_id = c.class_id
+            WHERE ta.teacher_id = @tid";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@tid", Session.TeacherDbId);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                cbClass.DataSource = dt;
+                cbClass.DisplayMember = "class_name";
+                cbClass.ValueMember = "class_id";
+            }
+        }
+        private void cbClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbClass.SelectedValue == null || cbClass.SelectedValue is DataRowView)
+                return;
+
+            int classId = Convert.ToInt32(cbClass.SelectedValue);
+            LoadStudents(classId);
+        }
+        private void LoadStudents(int classId)
+        {
+            dgvStudents.Rows.Clear();
+
+            using (SqlConnection conn =
+                new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True"))
+            {
+                conn.Open();
+
+                string sql = @"
+                    SELECT student_id,
+                           student_name,
+                           student_gender,
+                           student_address
+                    FROM students
+                    WHERE class_id = @cid";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@cid", classId);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    dgvStudents.Rows.Add(
+                        dr["student_id"],
+                        dr["student_name"],
+                        dr["student_gender"],
+                        dr["student_address"]
+                    );
+                }
+
+                dr.Close();
+            }
+        }
+        private void SetupStudentGrid()
+        {
+            dgvStudents.Columns.Clear();
+            dgvStudents.Columns.Add("colId", "Student ID");
+            dgvStudents.Columns.Add("colName", "Student Name");
+            dgvStudents.Columns.Add("colGender", "Gender");
+            dgvStudents.Columns.Add("colAddress", "Address");
+
+            dgvStudents.ColumnHeadersHeight = 45;
+            dgvStudents.EnableHeadersVisualStyles = false;
+
+            dgvStudents.ColumnHeadersDefaultCellStyle.BackColor =
+                Color.FromArgb(4, 87, 122);
+
+            dgvStudents.ColumnHeadersDefaultCellStyle.ForeColor =
+                System.Drawing.Color.White;
+
+            dgvStudents.ColumnHeadersDefaultCellStyle.Font =
+                new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+
+            dgvStudents.DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleCenter;
+        }
+    }
+}
